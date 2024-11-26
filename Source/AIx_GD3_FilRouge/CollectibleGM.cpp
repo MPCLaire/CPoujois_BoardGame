e81fa56dad93e999b99cb9c9659a39683fe6c229
MPCLaire <claire.poujois@gmail.com>
2024-11-26 18:23:39 +0100

    ajout fichiers projet
    J'ai recommencé un git parce que l'autre avait trop de bugs

---
+++ Source/AIx_GD3_FilRouge/CollectibleGM.cpp
@@ -0,0 +1,91 @@
+#include "CollectibleGM.h"
+#include "Blueprint/UserWidget.h"
+#include "Kismet/GameplayStatics.h"
+
+void ACollectibleGM::BeginPlay()
+{
+    Super::BeginPlay();
+
+    ScoreWidgetInstance = CreateWidget<UUserWidget>(GetWorld(), WB_ScoreCollectible);
+    ScoreWidgetInstance->AddToViewport();
+}
+
+void ACollectibleGM::GetScore(int32 Score)
+{
+    ScoreTotal = ScoreTotal + Score;
+
+    if (ScoreWidgetInstance)
+    {
+        UFunction* UpdateScoreFunc = ScoreWidgetInstance->FindFunction(FName("UpdateScore"));
+
+        if (UpdateScoreFunc)
+        {
+            struct FUpdateScoreParams
+            {
+                int32 NewScore;
+            };
+
+            FUpdateScoreParams Parms;
+            Parms.NewScore = ScoreTotal;
+
+            ScoreWidgetInstance->ProcessEvent(UpdateScoreFunc, &Parms);
+        }
+    }
+}
+
+void ACollectibleGM::StartTimer()
+{
+    RemainingTime = TimerDuration;
+    GetWorld()->GetTimerManager().SetTimer(TimerHandle, this, &ACollectibleGM::UpdateTimer, 1.0f, true);
+}
+
+void ACollectibleGM::UpdateTimer()
+{
+    if (RemainingTime > 0)
+    {
+        RemainingTime--;
+        UE_LOG(LogTemp, Warning, TEXT("Temps restant : %d secondes"), RemainingTime);
+
+        //Appelle la fonction UpdateTimer dans WB_ScoreCollectible
+        if (ScoreWidgetInstance)
+        {
+            UFunction* UpdateTimerFunc = ScoreWidgetInstance->FindFunction(FName("UpdateTimer"));
+
+            if (UpdateTimerFunc)
+            {
+                struct FUpdateTimerParams
+                {
+                    int32 NewTime;
+                };
+
+                FUpdateTimerParams Parms;
+                Parms.NewTime = RemainingTime;
+
+                ScoreWidgetInstance->ProcessEvent(UpdateTimerFunc, &Parms);
+            }
+        }
+    }
+    else
+    {
+        EndCollectibleInstance = CreateWidget<UUserWidget>(GetWorld(), WB_EndCollectible);
+        EndCollectibleInstance->AddToViewport();
+
+        if (EndCollectibleInstance)
+        {
+            UFunction* GetFinalScoreFunc = EndCollectibleInstance->FindFunction(FName("GetFinalScore"));
+
+            if (GetFinalScoreFunc)
+            {
+                struct FGetFinalScoreParams
+                {
+                    int32 FinalScore;
+                };
+
+                FGetFinalScoreParams Parms;
+                Parms.FinalScore = ScoreTotal;
+
+                EndCollectibleInstance->ProcessEvent(GetFinalScoreFunc, &Parms);
+            }
+        }
+    }
+}
---
+++ Source/AIx_GD3_FilRouge/CollectibleGM.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+#include "Blueprint/UserWidget.h"
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CollectibleGM.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AIX_GD3_FILROUGE_API ACollectibleGM : public AGameModeBase
+{
+	GENERATED_BODY()
+protected :
+	virtual void BeginPlay() override;
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void GetScore(int32 Score);
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 ScoreTotal;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Timer")
+	int32 TimerDuration = 10;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Timer")
+	int32 RemainingTime;
+
+	FTimerHandle TimerHandle; 
+
+	UFUNCTION(BlueprintCallable, Category = "Timer")
+	void StartTimer();
+
+	UFUNCTION(BlueprintCallable, Category = "Timer")
+	void UpdateTimer();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UI")
+	TSubclassOf<UUserWidget> WB_ScoreCollectible;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UI")
+	UUserWidget* ScoreWidgetInstance;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UI")
+	TSubclassOf<UUserWidget> WB_EndCollectible;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UI")
+	UUserWidget* EndCollectibleInstance;
+};
